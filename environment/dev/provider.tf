f11b51d18ca9623493d704ab45520c5231702520
omka1992 <127044732+omka1992@users.noreply.github.com>
2025-12-16 12:10:17 +0530

    6

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -5,10 +5,17 @@ terraform {
       version = "4.53.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "omkar"
+    storage_account_name = "omkarstor1"
+    container_name       = "omkarstorc"
+    key                  = "dev.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
-features {}
-subscription_id = "b4461466-1e6b-4be2-bb70-1e96a72a41c8"
+  features {}
+  subscription_id = "b4461466-1e6b-4be2-bb70-1e96a72a41c8"
 }
 
